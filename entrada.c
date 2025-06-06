0ff4b2b04abd24251944793c66ce18fb310265a7
Jose Torres <rogertorres1231@outlook.com>
2025-06-06 11:55:20 -0300

    Feat: Entrada e Saida de Dados

--- entrada.c
+++ entrada.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
+
 int main() {
- // Variáveis.
-    int idade;
-    float altura ;
-    float peso ;
+    // Variáveis
+    int idade, matricula;
+    float altura, peso;
     char nome[20];
-      
-// Entrada de Dados.
-    printf("Digite sua idade: ");
+
+    // Entrada de Dados
+    printf("Digite sua idade: \n");
     scanf("%d", &idade);
-    printf("Sua idade é: %d\n", idade);
-   
-    printf("Digite sua altura: ");
-    scanf("%.2f", &altura);
-    printf("Sua altura é: %.2f\n", altura);
+
+    printf("Digite seu Nome: \n");
+    scanf("%s", &nome);
+
+    printf("Digite sua Matricula: \n");
+    scanf("%d", &matricula);
+
+    printf("Digite sua Altura: \n");
+    scanf("%f", &altura);
+
+    printf("Digite seu Peso: \n");
+    scanf("%f", &peso);
+
+    // Saída de Dados
+    printf("Sua idade é: %d - sua Matricula: %d\n", idade, matricula);
+    printf("Sua Altura é: %.2f - e peso: %.2f\n", altura, peso);
+    printf("Seu Nome é: %s\n", nome);
+
+    return 0;
 }
