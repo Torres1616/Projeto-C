472285fdf98d8f252c8130faec797be1935e179e
Jose Torres <rogertorres1231@outlook.com>
2025-06-12 22:06:55 -0300

    1

---
+++ media.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void entradadedados() {
+    int nota1, nota2, nota3, media;
+
+    // Entrada de Dados
+    printf("***  Media do alunno ***\n");
+
+    printf("Digite a Primeira nota: ");
+    scanf("%d/n", &nota1);
+
+    printf("Digite a Segunda nota: ");
+    scanf("%d/n", &nota2);
+
+    printf("Digite a Terceira nota: ");
+    scanf("%d/n", &nota3);
+}
