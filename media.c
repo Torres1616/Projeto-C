a9ef3b2bbbaf4bc790a8f914df7393df52e559f9
Jose Torres <rogertorres1231@outlook.com>
2025-06-12 22:22:44 -0300

    Feat: Media aluno

--- media.c
+++ media.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
-void entradadedados() {
-    int nota1, nota2, nota3, media;
+int main () {
+    float nota1, nota2, nota3;
+    float media;
 
     // Entrada de Dados
-    printf("***  Media do alunno ***\n");
+    printf("***  Media do aluno ***\n");
 
-    printf("Digite a Primeira nota: ");
-    scanf("%d/n", &nota1);
+    printf("Digite a Primeira nota:\n");
+    scanf("%f", &nota1);
 
-    printf("Digite a Segunda nota: ");
-    scanf("%d/n", &nota2);
+    printf("Digite a Segunda nota:\n");
+    scanf("%f", &nota2);
 
-    printf("Digite a Terceira nota: ");
-    scanf("%d/n", &nota3);
+    printf("Digite a Terceira nota: \n");
+    scanf("%f", &nota3);
+
+    // Cálculo da média
+    media = (float) (nota1 + nota2 + nota3) / 3;
+
+    // Saída de Dados
+    printf("***  Resultado da Media ***\n");
+    printf("A media do aluno e: %.2f\n", media);
+
+    return 0;
 }
